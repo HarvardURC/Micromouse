d86d69a061522f92f57d4e8222e99dee3b02e6ed
Schiststorm <james_conant@college.harvard.edu>
2018-02-11 17:23:07 -0800

    Hey I finished the wall printing

--- 2017,2018/new_mouse/master/wall_progress/wall_progress.ino
+++ 2017,2018/new_mouse/master/wall_progress/wall_progress.ino
@@ -3,13 +3,22 @@
 #include <i2c_t3.h>
 #include <config.h>
 
+#define NORTH 8
+#define EAST 4
+#define SOUTH 2
+#define WEST 1
+
 std::vector<int> sensor_pins = {pins::tofLeft, pins::tofLeftDiag, pins::tofFront, pins::tofRightDiag, pins::tofRight};
 std::vector<VL6180X*> sensors = {new VL6180X, new VL6180X, new VL6180X, new VL6180X, new VL6180X};
 std::vector<String> sensor_names = {"left", "leftDiag", "front", "rightDiag", "right"};
 
-const int threshold = 300;
+const int threshold = 250;
+
+unsigned char virtualWalls[256];
 
 void initSensor(int pin, VL6180X *sensor, int address);
+void setBoundaryWalls();
+void printVirtualMaze();
 
 void setup() {
   // put your setup code here, to run once:
@@ -28,23 +37,22 @@ void setup() {
   for (unsigned int i = 0; i < sensor_pins.size(); i++) {
     initSensor(sensor_pins[i], sensors[i], i + 1);
   }
+
+  // SET UP BOUNDARY WALLS!!!!! :)
+  setBoundaryWalls();
+
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:  
-  Serial.print("Wall on the ");
+
   for (unsigned int i = 0; i < sensors.size(); i += 2) {
-//    Serial.print(sensor_names[i]);
-//    Serial.print(": ");
-    
     if (sensors[i]->readRangeContinuousMillimeters() < threshold) {
       
       Serial.print(sensor_names[i]);
       Serial.print(" ");
     }
-   
-//    Serial.print(sensors[i]->readRangeContinuousMillimeters());
-//    Serial.print(" ");
     if (sensors[i]->timeoutOccurred()) { Serial.print(" TIMEOUT"); }
   }
   Serial.println();
@@ -68,3 +76,102 @@ void initSensor(int pin, VL6180X *sensor, int address) {
     Serial.print(pin);
     Serial.println(" connected.");
 }
+
+void setBoundaryWalls() {
+  // NOTE: Top of the maze is set to the the 0th row
+  
+  // North
+  for (int i = 0; i < 16; i++) {
+    virtualWalls[i] |= NORTH;
+  }
+
+  // East
+  for (int i = 15; i < 256; i += 16) {
+    virtualWalls[i] |= EAST;
+  }
+
+  // South
+  for (int i = 240; i < 256; i++) {
+    virtualWalls[i] |= SOUTH;
+  }
+
+  // West
+  for (int i = 0; i < 256; i += 16) {
+    virtualWalls[i] |= WEST;
+  }
+}
+
+void printVirtualMaze() {
+  int check_north = 0;
+  int check_south = 0;
+ 
+  for (int row = 0; row < 16; row++) {
+    printVirtualColumn(row, true);
+    printVirtualColumn(row, false);
+  }
+
+  // print very bottom row
+  //printVirtualColumn()
+}
+
+// isPost boolean variables flags whether we are printing a row with vertical walls or horizontal walls
+void printVirtualColumn(int row, bool isPost) {
+  int check_north_wall = 0;
+  int check_south_wall = 0;
+  int check_east_wall = 0;
+  int check_west_wall = 0;
+
+  for (int col = 0; col < 16; col++) {
+    check_north_wall = 0;
+    check_south_wall = 0;
+    check_east_wall = 0;
+    check_west_wall = 0;
+    
+    if (isPost) {
+      Serial.print("+")
+      // check north wall in here 
+      check_north_wall = virtualWalls[row * 16 + col] & NORTH;
+
+      // check south wall in here
+      if (row > 0) {
+        check_south_wall = virtualWalls[(row - 1) * 16 + col] & SOUTH;
+      }
+      
+      if (check_north_wall == NORTH || check_south_wall == SOUTH) {
+        Serial.print("---");
+      } 
+      else {
+        Serial.print("   ");
+      }
+    }
+    else {
+      check_west_wall = virtualWalls[row * 16 + col] & WEST;
+
+      if (col > 0) {
+        check_east_wall = virtualWalls[row * 16 + col - 1] & EAST;
+      } 
+
+      if (check_west_wall == WEST || check_east_wall == EAST) {
+        Serial.print("|");
+      } else {
+        Serial.print(" ");
+      }
+      
+      Serial.print("   ");
+    }
+  }
+
+  if (isPost) {
+    Serial.print("+");
+  } else {
+    Serial.print("|");
+  }
+
+  Serial.println();
+}
+
+
+
+
+
+
