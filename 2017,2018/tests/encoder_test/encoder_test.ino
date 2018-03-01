31ddd886952d73c0aa4cb787a1059b3c6de5f778
kendallzhu <kzhu@college.harvard.edu>
2018-03-01 13:55:51 -0500

    residual changes to tests

--- 2017,2018/tests/encoder_test/encoder_test.ino
+++ 2017,2018/tests/encoder_test/encoder_test.ino
@@ -3,45 +3,46 @@
  * To check, manually spin wheels and see if values change properly
  */
 
+#include <VL6180X.h>
 #include <i2c_t3.h>
 #include <config.h>
-#include "motors.hh"
-#include "sensors.hh"
+#include <Encoder.h>
 
-using namespace pins;
-
-Motor* motorL;
-Motor* motorR;
-SensorArray* sensorArr;
+Encoder encoderLeft(pins::encoderL1, pins::encoderL2);
+Encoder encoderRight(pins::encoderR1, pins::encoderR2);
 
 void setup() {
-    // put your setup code here, to run once:
-    Serial.begin(9600);
-    Serial.print("Right + Left Encoder Test:");
-
-    motorL = new Motor(
-        motorPowerL,
-        motorDirectionL,
-        encoderL1,
-        encoderL2,
-        *sensorArr);
-
-    motorR = new Motor(
-        motorPowerR,
-        motorDirectionR,
-        encoderR1,
-        encoderR2,
-        *sensorArr);
-
-    pinMode(motorMode, OUTPUT);
-    digitalWrite(motorMode, HIGH);
+  // put your setup code here, to run once:
+  // quiet the buzzer
+  pinMode(pins::buzzer, OUTPUT);
+  digitalWrite(pins::buzzer, LOW);
+  
+  Serial.begin(9600);
+  Serial.print("Right + Left Encoder Test:");
 }
 
+long positionLeft  = -999;
+long positionRight = -999;
 
 void loop() {
-    Serial.print("Left encoder: ");
-    Serial.print(motorL->readTicks());
-    Serial.print(" Right encoder: ");
-    Serial.println(motorR->readTicks());
-    delay(20);
+  long newLeft, newRight;
+  newLeft = encoderLeft.read();
+  newRight = encoderRight.read();
+  if (newLeft != positionLeft || newRight != positionRight) {
+    Serial.print("Left = ");
+    Serial.print(newLeft);
+    Serial.print(", Right = ");
+    Serial.print(newRight);
+    Serial.println();
+    positionLeft = newLeft;
+    positionRight = newRight;
+  }
+  // if a character is sent from the serial monitor,
+  // reset both back to zero.
+  if (Serial.available()) {
+    Serial.read();
+    Serial.println("Reset both knobs to zero");
+    encoderLeft.write(0);
+    encoderRight.write(0);
+  }
 }
